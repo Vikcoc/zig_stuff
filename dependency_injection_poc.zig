5fde91b2e7de861f8d58baaf498734eeb6c71230
Vikcoc <vikcoc@yahoo.com>
2024-06-04 16:01:30 +0300

    Not working

--- dependency_injection_poc.zig
+++ dependency_injection_poc.zig
@@ -13,17 +13,52 @@ const Tst = struct {
     }
 };
 
+const Container = struct {
+    comptime services: std.AutoHashMap(type, std.builtin.Type.Fn) = .{},
+    pub fn addFactory(comptime this: *Container, comptime function: anytype) *Container {
+        const tp = this.services.getKey(@TypeOf(function).Fn.return_type.?);
+        if (tp != null)
+            @compileLog("Cannot insert 2 factory methods for the same type: {s}", .{@typeName(tp.?)});
+
+        inline for (@typeInfo(@TypeOf(function)).Fn.params) |param| {
+            const ptp = this.services.getKey(param.type.?);
+            if (ptp == null)
+                @compileLog("Cannot generate type {s}", .{@typeName(param.type.?)});
+        }
+        this.services.addOne(function);
+        return this;
+    }
+    pub fn inject(comptime this: *Container, comptime function: anytype) @typeInfo(@TypeOf(function)).Fn.return_type.? {
+        const tp = @typeInfo(@TypeOf(function));
+        comptime var argt: [tp.Fn.params.len]type = undefined;
+        inline for (tp.Fn.params, 0..) |param, index| {
+            const ptp = this.services.getKey(param.type.?);
+            if (ptp == null)
+                @compileLog("Cannot generate type {s}", .{@typeName(param.type.?)});
+            argt[index] = param.type.?;
+        }
+        const tptp = std.meta.Tuple(argt[0..]);
+        const fields = @typeInfo(tptp).Struct.fields;
+        const y: tptp = undefined;
+        inline for (fields) |field| {
+            const fun = this.services[field.type];
+            @field(y, field.name) = this.inject(fun);
+        }
+
+        return @call(std.builtin.CallModifier.auto, function, y);
+    }
+};
+
 pub fn main() void {
-    std.debug.print("This is returned {d}\n", .{doInvoke(extraReturn)});
-    doInvoke(extra);
+    comptime var cont: Container = .{};
+    cont.addFactory(fn () Tst{return .{}});
+    cont.addFactory(fn () Tst{return 20});
+    std.debug.print("This is returned {d}\n", .{cont.inject(extraReturn)});
+    cont.inject(extra);
 }
 
 pub fn doInvoke(comptime func: anytype) @typeInfo(@TypeOf(func)).Fn.return_type.? {
     const c = @typeInfo(@TypeOf(func));
-    switch (c) {
-        .Fn => {},
-        inline else => @compileLog("Cannot"),
-    }
     comptime var d: [c.Fn.params.len]type = undefined;
 
     inline for (c.Fn.params, 0..) |param, index| {
